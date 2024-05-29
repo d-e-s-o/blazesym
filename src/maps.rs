fbdf81ee535a20dff71fc547a8305c2b31b14d22
Daniel Müller <deso@posteo.net>
2024-05-29 14:05:52 -0700

    Join w/ root dir
    Signed-off-by: Daniel Müller <deso@posteo.net>

--- src/maps.rs
+++ src/maps.rs
@@ -193,8 +193,8 @@ fn parse_maps_line<'line>(line: &'line [u8], pid: Pid, root: &Path) -> Result<Ma
         [] => None,
         [b'/', ..] => {
             let symbolic_path =
-                bytes_to_path(path_str.strip_suffix(b" (deleted)").unwrap_or(path_str))
-                    .to_path_buf();
+                bytes_to_path(path_str.strip_suffix(b" (deleted)").unwrap_or(path_str));
+            let symbolic_path = root.join(symbolic_path);
             // TODO: May have to resolve the symbolic link in case of
             //       `Pid::Slf` here for remote symbolization use cases.
             let maps_file = PathBuf::from(format!(
