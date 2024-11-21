1e9011081df4e860db126b2193906e4e0e4f895d
Daniel Müller <deso@posteo.net>
2024-11-21 09:55:32 -0800

    Start

--- src/elf/parser.rs
+++ src/elf/parser.rs
@@ -222,7 +222,9 @@ impl<'mmap> SymbolTableCache<'mmap> {
 }
 
 
-struct Cache<'bcknd> {
+struct Cache<'bcknd, B> {
+    /// The backend being used for reading ELF data.
+    backend: B,
     /// A slice of the raw ELF data that we are about to parse.
     elf_data: &'bcknd [u8],
     /// The cached ELF header.
@@ -238,10 +240,11 @@ struct Cache<'bcknd> {
     dynsym: OnceCell<SymbolTableCache<'bcknd>>,
 }
 
-impl<'bcknd> Cache<'bcknd> {
+impl<'bcknd, B> Cache<'bcknd, B> {
     /// Create a new `Cache` using the provided raw ELF object data.
-    fn new(elf_data: &'bcknd [u8]) -> Self {
+    fn new(backend: B, elf_data: &'bcknd [u8]) -> Self {
         Self {
+            backend,
             elf_data,
             ehdr: OnceCell::new(),
             shdrs: OnceCell::new(),
@@ -561,7 +564,7 @@ impl<'bcknd> Cache<'bcknd> {
     }
 }
 
-impl Debug for Cache<'_> {
+impl<B> Debug for Cache<'_, B> {
     fn fmt(&self, f: &mut Formatter<'_>) -> FmtResult {
         write!(f, "Cache")
     }
@@ -761,7 +764,7 @@ pub(crate) struct ElfParser<B = Mmap> {
     //         this member. Rather, they should never outlive `self`.
     //         Furthermore, this member has to be listed before `_mmap`
     //         to make sure we never end up with a dangling reference.
-    cache: Cache<'static>,
+    cache: Cache<'static, B>,
     /// A mapping from section index to decompressed section data.
     // Note that conceptually this member would be best contained in the
     // `Cache` type, however, lifetimes get very hairy once we move it
@@ -772,6 +775,24 @@ pub(crate) struct ElfParser<B = Mmap> {
     path: Option<PathBuf>,
     /// The backend used.
     _backend: B,
+}
+
+impl ElfParser<File> {
+    pub(crate) fn __open__file<P>(file: File, path: P) -> Self
+    where
+        P: Into<PathBuf>,
+    {
+        let elf_data = &[];
+
+        let parser = Self {
+            decompressed: InsertMap::new(),
+            // XXX: Cloning should be avoided.
+            cache: Cache::new(file.try_clone().unwrap(), elf_data),
+            path: Some(path.into()),
+            _backend: file,
+        };
+        parser
+    }
 }
 
 impl ElfParser<Mmap> {
@@ -792,11 +813,11 @@ impl ElfParser<Mmap> {
         //         data.
         let elf_data = unsafe { mem::transmute::<&[u8], &'static [u8]>(mmap.deref()) };
 
-        let parser = ElfParser {
-            _backend: mmap,
+        let parser = Self {
             decompressed: InsertMap::new(),
-            cache: Cache::new(elf_data),
+            cache: Cache::new(mmap.clone(), elf_data),
             path,
+            _backend: mmap,
         };
         parser
     }
